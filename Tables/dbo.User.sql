59f3eea77cf07a8ebf70e95c30adee5bf3262a22
jfibrahim <jibrahim@jiosdev.com>
2017-06-17 10:42:13 +0200

    add users table to TestBranch1

---
+++ Tables/dbo.User.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[User]
+(
+[ID] [int] NOT NULL,
+[FirstName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[LastName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
